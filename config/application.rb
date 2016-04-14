2e057442f1858caaec01fa194aaeff4e6a5b8637
Kyle Scully <scullykns@gmail.com>
2016-04-13 20:25:26 -0700

    load mongoid

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Raceday
   class Application < Rails::Application
+    # bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
+
+    # which default ORM are we using with scaffold
+    # add --orm mongoid, or active_record
+    #  to rails generate cmd line to be specific
+    # config.generators {|g| g.orm :active_record}
+
+    config.generators {|g| g.orm :mongoid}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
