6da85a393210e9966f839655ed6c6309921e6f03
Kyle Scully <scullykns@gmail.com>
2016-04-14 21:55:27 -0700

    rspec spec/scaffold_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :racers
+  root to: 'racers#index'
 end
