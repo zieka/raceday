9596d6fba5a18c1289b04c0d44408b0edb3cb58a
Kyle Scully <scullykns@gmail.com>
2016-04-14 20:43:51 -0700

    rspec spec/crud_spec.rb -e rq03

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,7 @@
 class Racer
-  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
   include Mongoid::Document
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
 
   #database client
   def self.mongo_client
@@ -25,6 +26,17 @@ class Racer
       result = result.limit(limit)
     end
     result
+  end
+
+  #accept parameters to set id, number, first_name, last_name, gender, group, secs
+  def initialize(params={})
+    @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group=params[:group]
+    @secs=params[:secs].to_i
   end
 
 end
